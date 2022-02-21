33e85209e8602e4c7757d4f4f6069b60fdbbb717
Abdelhamid Khamis <53490154+Abdelhamid-khamis@users.noreply.github.com>
2022-02-22 01:49:48 +0200

    addded segmentation section

---
+++ Section 5 - Supply chain statistical analysis/scatter.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.Dataframe(Data)
+df.plot(kind='bar',x="dataframe_1",y="dataframe_2") # bar can be replaced by 
+# scatter or line or even left as default
+plt.show()
